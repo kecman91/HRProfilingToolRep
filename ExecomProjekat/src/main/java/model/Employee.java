575dee146aa087731593def1fd501bf1ef0d1d38
Gavrilo <Gavrilo@Gavrilo-PC>
2015-07-24 15:04:37 +0200

    Employee

--- ExecomProjekat/src/main/java/model/Employee.java
+++ ExecomProjekat/src/main/java/model/Employee.java
@@ -1,7 +1,249 @@
 package model;
 
+import java.util.Date;
+
 public class Employee {
 	
-	private int idproject;
-
+	private int idEmployee;
+	private String nameEmployee;
+	private String gender;
+	private Date dateOfBirth;
+	private String address;
+	private int phoneNumber;
+	private String email;
+	private int emergencyPhoneNumber;
+	private Date startDate;
+	private Date endDate;
+	private Date startDateFromBooklet;
+	private String username;
+	private int idCardNumber;
+	private int passportNumber;
+	private int yearsOfWorkingExpInExecom;
+	private int yearsOfWorking;
+	private String placeOfBirth;
+	private String trainingLearningPriority;
+	private String licencesCertificates;
+	private String awards;
+	private String contractType;
+	private int communication;
+	private int fastLearning;
+	private int openToChange;
+	private int teamPlayer;
+	private int proactiveCommunication;
+	private int interpersonalSkills;
+	private int knowledgeSharing;
+	private int judgement;
+	private int decisionMaking;
+	private int influencing;
+	private int leadership;
+	private int coaching;
+	private int organizationalSkills;
+	public int getIdEmployee() {
+		return idEmployee;
+	}
+	public void setIdEmployee(int idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+	public String getNameEmployee() {
+		return nameEmployee;
+	}
+	public void setNameEmployee(String nameEmployee) {
+		this.nameEmployee = nameEmployee;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getEmergencyPhoneNumber() {
+		return emergencyPhoneNumber;
+	}
+	public void setEmergencyPhoneNumber(int emergencyPhoneNumber) {
+		this.emergencyPhoneNumber = emergencyPhoneNumber;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public Date getStartDateFromBooklet() {
+		return startDateFromBooklet;
+	}
+	public void setStartDateFromBooklet(Date startDateFromBooklet) {
+		this.startDateFromBooklet = startDateFromBooklet;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getIdCardNumber() {
+		return idCardNumber;
+	}
+	public void setIdCardNumber(int idCardNumber) {
+		this.idCardNumber = idCardNumber;
+	}
+	public int getPassportNumber() {
+		return passportNumber;
+	}
+	public void setPassportNumber(int passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+	public int getYearsOfWorkingExpInExecom() {
+		return yearsOfWorkingExpInExecom;
+	}
+	public void setYearsOfWorkingExpInExecom(int yearsOfWorkingExpInExecom) {
+		this.yearsOfWorkingExpInExecom = yearsOfWorkingExpInExecom;
+	}
+	public int getYearsOfWorking() {
+		return yearsOfWorking;
+	}
+	public void setYearsOfWorking(int yearsOfWorking) {
+		this.yearsOfWorking = yearsOfWorking;
+	}
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
+	public String getTrainingLearningPriority() {
+		return trainingLearningPriority;
+	}
+	public void setTrainingLearningPriority(String trainingLearningPriority) {
+		this.trainingLearningPriority = trainingLearningPriority;
+	}
+	public String getLicencesCertificates() {
+		return licencesCertificates;
+	}
+	public void setLicencesCertificates(String licencesCertificates) {
+		this.licencesCertificates = licencesCertificates;
+	}
+	public String getAwards() {
+		return awards;
+	}
+	public void setAwards(String awards) {
+		this.awards = awards;
+	}
+	public String getContractType() {
+		return contractType;
+	}
+	public void setContractType(String contractType) {
+		this.contractType = contractType;
+	}
+	public int getCommunication() {
+		return communication;
+	}
+	public void setCommunication(int communication) {
+		this.communication = communication;
+	}
+	public int getFastLearning() {
+		return fastLearning;
+	}
+	public void setFastLearning(int fastLearning) {
+		this.fastLearning = fastLearning;
+	}
+	public int getOpenToChange() {
+		return openToChange;
+	}
+	public void setOpenToChange(int openToChange) {
+		this.openToChange = openToChange;
+	}
+	public int getTeamPlayer() {
+		return teamPlayer;
+	}
+	public void setTeamPlayer(int teamPlayer) {
+		this.teamPlayer = teamPlayer;
+	}
+	public int getProactiveCommunication() {
+		return proactiveCommunication;
+	}
+	public void setProactiveCommunication(int proactiveCommunication) {
+		this.proactiveCommunication = proactiveCommunication;
+	}
+	public int getInterpersonalSkills() {
+		return interpersonalSkills;
+	}
+	public void setInterpersonalSkills(int interpersonalSkills) {
+		this.interpersonalSkills = interpersonalSkills;
+	}
+	public int getKnowledgeSharing() {
+		return knowledgeSharing;
+	}
+	public void setKnowledgeSharing(int knowledgeSharing) {
+		this.knowledgeSharing = knowledgeSharing;
+	}
+	public int getJudgement() {
+		return judgement;
+	}
+	public void setJudgement(int judgement) {
+		this.judgement = judgement;
+	}
+	public int getDecisionMaking() {
+		return decisionMaking;
+	}
+	public void setDecisionMaking(int decisionMaking) {
+		this.decisionMaking = decisionMaking;
+	}
+	public int getInfluencing() {
+		return influencing;
+	}
+	public void setInfluencing(int influencing) {
+		this.influencing = influencing;
+	}
+	public int getLeadership() {
+		return leadership;
+	}
+	public void setLeadership(int leadership) {
+		this.leadership = leadership;
+	}
+	public int getCoaching() {
+		return coaching;
+	}
+	public void setCoaching(int coaching) {
+		this.coaching = coaching;
+	}
+	public int getOrganizationalSkills() {
+		return organizationalSkills;
+	}
+	public void setOrganizationalSkills(int organizationalSkills) {
+		this.organizationalSkills = organizationalSkills;
+	}
+	
+	
+	
+	
 }
