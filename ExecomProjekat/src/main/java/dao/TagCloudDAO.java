2da8bf4573b79f417400193d2fb98ae2e22b8412
praktikant3 <praktikant3@praktikant3>
2015-07-27 11:22:26 +0200

    Izbacen bespotreban import

--- ExecomProjekat/src/main/java/dao/TagCloudDAO.java
+++ ExecomProjekat/src/main/java/dao/TagCloudDAO.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import model.Project;
 import model.TagCloud;
 
 @Repository
