b8f2e4108cce0e0c62bd5da1df380854ce1be204
Gavrilo <Gavrilo@Gavrilo-PC>
2015-07-27 16:52:57 +0200

    Build.gradle

--- ExecomProjekat/src/main/java/app/Application.java
+++ ExecomProjekat/src/main/java/app/Application.java
@@ -1,15 +1,23 @@
 package app;
 
-import java.util.Arrays;
+import model.TagCloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
+import repository.TagCloudRepository;
 
 @SpringBootApplication
 public class Application {
+	@Autowired
+    TagCloudRepository repository;
 	public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		
+		SpringApplication.run(Application.class);
+		
+		
+        /*ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
 
@@ -17,6 +25,39 @@ public class Application {
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
-        }
+        }*/
+		
+		
+		
+		
     }
+	 public void run(String... strings) throws Exception {
+		 TagCloud tc = new TagCloud();
+		 
+		 tc.setNameTagCloud("Java");
+		 tc.setTipTagCloud("programming language");
+		 repository.save( tc);
+		 
+		 tc = new TagCloud();
+		 
+		 tc.setNameTagCloud("C#");
+		 tc.setTipTagCloud("programming language");
+		 repository.save( tc);
+		 
+		 tc = new TagCloud();
+		 
+		 tc.setNameTagCloud("C++");
+		 tc.setTipTagCloud("programming language");
+		 repository.save( tc);
+		 
+		 	System.out.println("tcs found with findAll():");
+	        System.out.println("-------------------------------");
+	        for (TagCloud tc1 : repository.findAll()) {
+	            System.out.println(tc1.getNameTagCloud() + " ID:  " + tc1.getIdTagCloud());
+	        }
+	        System.out.println();
+		 
+		 
+	 }
+	
 }
