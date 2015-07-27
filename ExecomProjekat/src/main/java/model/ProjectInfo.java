660a074920657a791948f2c31dfcdf057357a98b
praktikant1 <praktikant1@Praktikant1>
2015-07-27 15:04:14 +0200

    ajde malaa

--- ExecomProjekat/src/main/java/model/ProjectInfo.java
+++ ExecomProjekat/src/main/java/model/ProjectInfo.java
@@ -6,10 +6,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
  
 @Entity
-@Table(name="PROJECTINFO")
 public class ProjectInfo {
 	
 	@Id
--- ExecomProjekat/src/main/java/model/TagCloud.java
+++ ExecomProjekat/src/main/java/model/TagCloud.java
@@ -2,27 +2,21 @@ package model;
 
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="TAGCLOUD")
 public class TagCloud {
 	
 	@Id
-	@Column(name="IDTAGCLOUD")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idTagCloud;
 	
-	@Column(name="NAME_TAGCLOUD")
 	private String nameTagCloud;
 	
-	@Column(name="TIP_TAGCLOUD")
 	private String tipTagCloud;
 	
 	@OneToMany(mappedBy="tagCloud")
