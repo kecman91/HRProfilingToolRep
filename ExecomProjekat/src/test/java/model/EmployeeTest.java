eb70fc0b3b3a019408fd5c303f3f4f812e928005
praktikant1 <praktikant1@Praktikant1>
2015-07-27 13:23:06 +0200

    EmployeeTest

---
+++ ExecomProjekat/src/test/java/model/EmployeeTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	@Test
+	public void testCreateEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmployee() {
+		fail("Not yet implemented");
+	}
+
+}
